a431041b0cb87a88347f47e578dc8fcb058898a1
skyblue0110 <122327038+skyblue0110@users.noreply.github.com>
2023-02-15 14:57:55 +0900

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,12 +1,12 @@
-$('.article1').slick({
-    autoplay:true,
-    autoplaySpeed:5000,
-    dots:true,
-    arrows:true,
-    pauseOnFocus:false,    // 화살표나 도트버튼 클릭시 자동실행 정지안함
-    prevArrow:'<button class="slick-prev slick-arrow"><i class="fa-solid fa-angle-left"></i></button>' ,
-    nextArrow:'<button class="slick-next slick-arrow"><i class="fa-solid fa-angle-right"></i></button>'
-})
+// $('.article1').slick({
+//     autoplay:true,
+//     autoplaySpeed:5000,
+//     dots:true,
+//     arrows:true,
+//     pauseOnFocus:false,    // 화살표나 도트버튼 클릭시 자동실행 정지안함
+//     prevArrow:'<button class="slick-prev slick-arrow"><i class="fa-solid fa-angle-left"></i></button>' ,
+//     nextArrow:'<button class="slick-next slick-arrow"><i class="fa-solid fa-angle-right"></i></button>'
+// })
 
 $('.article1 .plpa i').on('click', function(){
     if ( $(this).hasClass('fa-pause') ) {
